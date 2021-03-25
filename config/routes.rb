3cee50a3a3351fc75670f388d51367b2cec4e171
Pedro Ramirez <prlara2000@gmail.com>
2021-03-25 13:30:22 -0500

    fixing the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home'
 
-  get '/pages' => 'pages#home'
+  get '/home' => 'pages#home'
   get '/about' => 'pages#about'
   get '/thanks' => 'pages#thanks'
 end
